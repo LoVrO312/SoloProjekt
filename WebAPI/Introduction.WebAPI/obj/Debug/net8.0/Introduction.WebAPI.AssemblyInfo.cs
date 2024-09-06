bca772cb9a93428b8e81b3a15c5327cf18fb36af
Lovro <lovro.baric111@gmail.com>
2024-09-06 13:52:41 +0200

    no message

--- WebAPI/Introduction.WebAPI/obj/Debug/net8.0/Introduction.WebAPI.AssemblyInfo.cs
+++ WebAPI/Introduction.WebAPI/obj/Debug/net8.0/Introduction.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Introduction.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd70c15ce6b9ba92d59c4f5d25e7f7ad6506466f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Introduction.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Introduction.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
